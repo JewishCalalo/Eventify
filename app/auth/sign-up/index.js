143ab6e53aff95ed43fa9efb28d2f3aad23dc4f0
JewishCalalo <111262147+JewishCalalo@users.noreply.github.com>
2024-09-13 18:52:18 +0800

    13/09/2024
    Account Registration Notification

--- app/auth/sign-up/index.js
+++ app/auth/sign-up/index.js
@@ -34,7 +34,11 @@ export default function SignUp() {
   .then((userCredential) => {
     // Signed up 
     const user = userCredential.user;
-    router.replace('/home')
+    router.replace('auth/sign-in')
+    {
+        ToastAndroid.show('Account Succsessfully Registered',ToastAndroid.SHORT)
+        return;
+      }
     console.log=(user);
     // ...
   })
